d5107ae91057443d16f0f1f507c6ec2ef31a00bd
purpleicious <jacksarick@gmail.com>
2015-10-18 20:11:36 -0400

    It works!

--- scripts/hide.js
+++ scripts/hide.js
@@ -1,1 +1,4 @@
-console.log("It works");
+// add hide/unhide buttons
+$( ".flat-list.buttons" ).append("<li><span class='option main active'><a href='javascript:void(0)' class='togglebutton savelink' onclick='hideOn(this)'>hide</a></span></li>");
+
+$( "<span class='separator'>|</span><a href='javascript:void(0)' onclick='hideOff()' title='Unhide all hidden things'>unhide</a>" ).insertAfter($(".user").first());
